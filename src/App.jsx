96543fc6960c02674946e0a57055d9ce04a4a19b
ben wozak <benwozak@bens-MacBook-Pro-3.local>
2024-08-26 11:50:04 -0600

    fixed pwa icon compatability

--- src/App.jsx
+++ src/App.jsx
@@ -21,29 +21,8 @@ function App() {
 
   return (
     <Router>
-      <Helmet>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="theme-color" content="#000000" />
-        <meta name="description" content="Description of your PWA" />
-
-        {/* <!-- iOS meta tags --> */}
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="black" />
-        <meta name="apple-mobile-web-app-title" content="PWA Title" />
-
-        {/* <!-- Microsoft Tiles --> */}
-        <meta name="msapplication-TileColor" content="#000000" />
-        <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
-
-        {/* <!-- Favicon and Apple Touch Icons --> */}
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-
-        {/* <!-- Web App Manifest --> */}
-        <link rel="manifest" href="/manifest.json" />
-      </Helmet>
+      {/* <Helmet>
+      </Helmet> */}
       <Suspense fallback={<Loading />}>
         <Routes>
           <Route path="/" element={<Layout />}>
